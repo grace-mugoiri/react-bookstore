3b1bbc3c3a150eecdaefada6e2620f263472f500
grace-mugoiri <gracemugoiri@gmail.com>
2020-01-22 08:09:53 +0300

    add action directory that defines redux actions

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+export const createBook = (book) => ({
+  type: 'CREATE_BOOK', book,
+});
+
+export const deleteBook = (book) => ({
+  type: 'REMOVE_BOOK', book,
+});
